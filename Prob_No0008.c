c2edb1e2d3f3ce675f893afe414462ba3d7e85ef
Sangyeon <arsene96@naver.com>
2017-01-17 19:13:42 +0900

    Add Files

---
+++ Prob_No0008.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main(void) {
+	printf("%c%c%c\n%c%c%c\n%c%c%c", 1, 22, 2, 25, 16, 23, 3, 21, 4);
+
+	return 0;
+}
---
+++ Prob_No0009.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main(void) {
+	printf("#include<stdio.h>\nmain()\n{\n\tprintf(\"Hello World\\n\");\n}");
+
+	return 0;
+}
